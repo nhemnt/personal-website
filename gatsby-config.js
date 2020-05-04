ee92ab279fd3e3e3b60d1a5cc733e09aa93e8446
hemant <nhemnt@gmail.com>
2020-05-04 13:30:00 +0530

    add colors to function in pre code using gatsby prism

--- gatsby-config.js
+++ gatsby-config.js
@@ -78,5 +78,35 @@ module.exports = {
       },
     },
     `gatsby-plugin-dark-mode`,
+    {
+      resolve: `gatsby-remark-prismjs`,
+      options: {
+        classPrefix: "language-",
+        inlineCodeMarker: null,
+        aliases: {},
+        showLineNumbers: false,
+        noInlineHighlight: false,
+        languageExtensions: [
+          {
+            language: "superscript",
+            extend: "javascript",
+            definition: {
+              superscript_types: /(SuperType)/,
+            },
+            insertBefore: {
+              function: {
+                superscript_keywords: /(superif|superelse)/,
+              },
+            },
+          },
+        ],
+        prompt: {
+          user: "root",
+          host: "localhost",
+          global: false,
+        },
+        escapeEntities: {},
+      },
+    },
   ],
 }
