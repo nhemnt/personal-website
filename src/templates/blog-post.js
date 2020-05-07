12f3c6d87d99556ef68c0fc7df8641046b3a19a2
hemant <nhemnt@gmail.com>
2020-05-08 01:15:51 +0530

    add style to blod post page

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,69 +1,89 @@
-import React from 'react'
-import { Link, graphql } from 'gatsby'
+import React from "react";
+import { Link, graphql } from "gatsby";
 
-import Bio from '../components/Bio'
-import Layout from '../components/Layout'
-import SEO from '../components/seo'
-import { rhythm, scale } from '../utils/typography'
-
+import Bio from "../components/Bio";
+import Layout from "../components/Layout";
+import SEO from "../components/seo";
+import { rhythm, scale } from "../utils/typography";
+import { Header, Blog } from "./styles";
+import PostHeader from "./post-header.jpg";
 class BlogPostTemplate extends React.Component {
   render() {
-    const post = this.props.data.markdownRemark
-    const siteTitle = this.props.data.site.siteMetadata.title
-    const { previous, next } = this.props.pageContext
+    const post = this.props.data.markdownRemark;
+    const { siteTitle, author } = this.props.data.site.siteMetadata;
+    const { previous, next } = this.props.pageContext;
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <SEO title={post.frontmatter.title} description={post.excerpt} />
-        <h1>{post.frontmatter.title}</h1>
-        <p
-          style={{
-            ...scale(-1 / 5),
-            display: `block`,
-            marginBottom: rhythm(1),
-            marginTop: rhythm(-1),
-          }}
-        >
-          {post.frontmatter.date}
-        </p>
-        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+
+        <Blog>
+          <Header style={{ backgroundImage: `url(${PostHeader})` }}>
+            <div className="overlay" />
+            <div className="container">
+              <div className="row">
+                <div className="col-lg-8 col-md-10 mx-auto">
+                  <div className="post-heading">
+                    <h1>{post.frontmatter.title}</h1>
+                    <h2 className="subheading"></h2>
+                    <span className="meta">
+                      Posted by <a href="#">{author}</a> on{" "}
+                      {post.frontmatter.date}
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Header>
+          <article>
+            <div className="container">
+              <div className="row">
+                <div className="col-lg-8 col-md-10 mx-auto">
+                  <div dangerouslySetInnerHTML={{ __html: post.html }} />
+                </div>
+              </div>
+            </div>
+          </article>
+          <hr />
+
+          <ul
+            style={{
+              display: `flex`,
+              flexWrap: `wrap`,
+              justifyContent: `space-between`,
+              listStyle: `none`,
+              padding: 0,
+            }}
+          >
+            <li>
+              {previous && (
+                <Link to={previous.fields.slug} rel="prev">
+                  ← {previous.frontmatter.title}
+                </Link>
+              )}
+            </li>
+            <li>
+              {next && (
+                <Link to={next.fields.slug} rel="next">
+                  {next.frontmatter.title} →
+                </Link>
+              )}
+            </li>
+          </ul>
+        </Blog>
+
         <hr
           style={{
             marginBottom: rhythm(1),
           }}
         />
         <Bio />
-
-        <ul
-          style={{
-            display: `flex`,
-            flexWrap: `wrap`,
-            justifyContent: `space-between`,
-            listStyle: `none`,
-            padding: 0,
-          }}
-        >
-          <li>
-            {previous && (
-              <Link to={previous.fields.slug} rel="prev">
-                ← {previous.frontmatter.title}
-              </Link>
-            )}
-          </li>
-          <li>
-            {next && (
-              <Link to={next.fields.slug} rel="next">
-                {next.frontmatter.title} →
-              </Link>
-            )}
-          </li>
-        </ul>
       </Layout>
-    )
+    );
   }
 }
 
-export default BlogPostTemplate
+export default BlogPostTemplate;
 
 export const pageQuery = graphql`
   query BlogPostBySlug($slug: String!) {
@@ -83,4 +103,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
