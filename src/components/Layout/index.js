a777780553e2a730ead5c5987d68482fdcd97de2
hemant <nhemnt@gmail.com>
2020-05-08 02:22:50 +0530

    remove loadable dependecy from project

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
-import loadable from '@loadable/component';
-
-const OtherComponent = loadable(() => import('./Child'))
+// import loadable from '@loadable/component';
+import Child from './Child';
+// const OtherComponent = loadable(() => import('./Child'))
 const Layout = (props) => {
   
     return (
       <>
-         <OtherComponent {...props} />
+         <Child {...props} />
       </>
     )
 }
