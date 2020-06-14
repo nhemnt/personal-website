52693d60362c28cef40b3a0c235d77b5f3ddbc14
Hemant Negi <nhemnt@gmail.com>
2020-06-14 14:30:30 +0530

    fix pagination for blog page

--- src/components/Pagination/index.jsx
+++ src/components/Pagination/index.jsx
@@ -1,32 +1,39 @@
 import React from 'react'
-import { Link } from "gatsby";
-import { Body } from './styles';
-// color: i + 1 === currentPage ? "#ffffff" : "",
-const Pagination = ({isFirst, prevPage, numPages, currentPage, isLast, nextPage, tag =''}) => (
+import { Link } from 'gatsby'
+import { Body } from './styles'
+
+const Pagination = ({
+  isFirst,
+  prevPage,
+  numPages,
+  currentPage,
+  isLast,
+  nextPage,
+  tag = '',
+}) => (
   <Body>
-  <ul>
-  {!isFirst && (
-    <Link to={prevPage} rel="prev">
-      <li>←</li>
-    </Link>
-  )}
-  {Array.from({ length: numPages }, (_, i) => (
-    
-      <Link
-      key={`pagination-number${i + 1}`}
-      to={`blog/${tag && ("tags/" + tag + "/") }${i === 0 ? "" : i + 1}`}
-      className={i + 1 === currentPage  ?"is-active" :""}
-      >
-        <li>{i + 1}</li>
-      </Link>
-  ))}
-  {!isLast && (
-    <Link to={nextPage} rel="next">
-      <li>→</li>
-    </Link>
-  )}
+    <ul>
+      {!isFirst && (
+        <Link to={prevPage} rel="prev">
+          <li>←</li>
+        </Link>
+      )}
+      {Array.from({ length: numPages }, (_, i) => (
+        <Link
+          key={`pagination-number${i + 1}`}
+          to={`/blog/${tag && 'tags/' + tag + '/'}${i === 0 ? '' : i + 1}`}
+          className={i + 1 === currentPage ? 'is-active' : ''}
+        >
+          <li>{i + 1}</li>
+        </Link>
+      ))}
+      {!isLast && (
+        <Link to={nextPage} rel="next">
+          <li>→</li>
+        </Link>
+      )}
     </ul>
-    </Body>
+  </Body>
 )
 
 export default Pagination
