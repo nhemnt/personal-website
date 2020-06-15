59bbc6ca6d72bf5626442438ec2a78a5a5ae0f76
Hemant Negi <nhemnt@gmail.com>
2020-06-16 01:46:23 +0530

    remoe window to fix build on netlify

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,13 +1,13 @@
 const isSSR = typeof window !== undefined;
 export const getItem = (item) => {
     if(isSSR){
-        return window.localStorage.getItem(item);
+        return localStorage.getItem(item);
     }
 }
 
 
 export const setItem = (name, value) => {
     if(isSSR){
-        window.localStorage.setItem(name, value);
+        localStorage.setItem(name, value);
     }
 }
