ab50a4f4a65f2225c020d79df8acbc57444ebabc
Hemant Negi <nhemnt@gmail.com>
2020-06-16 01:09:49 +0530

    remove zoom  in effect from project section

--- src/components/Homepage/styles.js
+++ src/components/Homepage/styles.js
@@ -147,34 +147,7 @@ export const ProjectSection = styled.section(
       transition: 1s;
       cursor: pointer;
     }
-    .card:hover {
-      transform: scale(1.05);
-      box-shadow: 10px 10px 15px rgba(0, 0, 0, 0.3);
-    }
-    .card::before,
-    .card::after {
-      position: absolute;
-      top: 0;
-      right: 0;
-      bottom: 0;
-      left: 0;
-      transform: scale3d(0, 0, 1);
-      transition: transform 0.3s ease-out 0s;
-      content: '';
-      pointer-events: none;
-    }
-    .card::before {
-      transform-origin: left top;
-    }
-    .card::after {
-      transform-origin: right bottom;
-    }
-    .card:hover::before,
-    .card:hover::after,
-    .card:focus::before,
-    .card:focus::after {
-      transform: scale3d(1, 1, 1);
-    }
+   
     .card-img {
       height: 200px;
     }
