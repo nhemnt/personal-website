238e438bf95fbd3fc2e5d59c00e8a53c8d5e582a
hemant negi <nhemnt@gmail.com>
2021-02-10 02:16:06 +0530

    remove sentry script

--- src/components/seo.js
+++ src/components/seo.js
@@ -60,9 +60,7 @@ function SEO({ description, lang, meta, keywords, title }) {
                   : []
               )
               .concat(meta)}
-          >
-            <script src={withPrefix('script.js')} type="text/javascript" />
-          </Helmet>
+          />
         )
       }}
     />
