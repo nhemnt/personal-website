232b08af6b08c0265d287502126ff15d6be9454b
hemant negi <nhemnt@gmail.com>
2021-05-24 21:12:26 +0530

    add seo tags to index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import Layout from '../components/Layout'
-import Homepage from "../components/Homepage";
+import Homepage from '../components/Homepage'
+import SEO from '../components/seo'
 const index = () => {
-  return(
+  return (
     <Layout>
-      <Homepage/>
+      <SEO
+        title={'Hemant Negi'}
+        keywords={[
+          'Hemant Negi',
+          'Senior Software Engineer',
+          'React js',
+          'nhemnt',
+          'Frontend',
+          'Javascript',
+        ]}
+      />
+      <Homepage />
     </Layout>
   )
 }
